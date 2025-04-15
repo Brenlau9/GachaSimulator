8c26b16b1630b1933b4a305cfc53613a33d266f4
Brendan Lau <brendanlau129@gmail.com>
2025-04-15 16:01:33 -0700

    update character pool

--- GachaSimulator/src/data/characterPool.js
+++ GachaSimulator/src/data/characterPool.js
@@ -1,20 +1,89 @@
 export const characterPool = [
+    // 5 Stars
     {
       name: "5 Star",
       rarity: 5,
       image: "/images/5star.png",
       rate: 0.01
     },
+    // 4 Stars
     {
-      name: "4 Star",
+      name: "4 Star 0",
       rarity: 4,
       image: "/images/4star.png",
+      rate: 0.03
+    },
+    {
+      name: "4 Star 1",
+      rarity: 4,
+      image: "/images/4star.png",
+      rate: 0.03
+    },
+    {
+      name: "4 Star 2",
+      rarity: 4,
+      image: "/images/4star.png",
+      rate: 0.03
+    },
+    // 3 Stars 
+    {
+      name: "3 Star 0",
+      rarity: 3,
+      image: "/images/3star.png",
       rate: 0.09
     },
     {
-      name: "3 Star",
+      name: "3 Star 1",
       rarity: 3,
       image: "/images/3star.png",
-      rate: 0.90
+      rate: 0.09
+    },
+    {
+      name: "3 Star 2",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 3",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 4",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 5",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 6",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 7",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 8",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
+    },
+    {
+      name: "3 Star 9",
+      rarity: 3,
+      image: "/images/3star.png",
+      rate: 0.09
     }
   ];
